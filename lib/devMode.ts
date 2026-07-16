96020d4c151173ce9666232fc468c35192855463
SAMAHASSAN000 <samahassan000@gmail.com>
2026-07-16 09:32:03 +0300

    dev

--- lib/devMode.ts
+++ lib/devMode.ts
@@ -15,4 +15,4 @@
 export const DEV_WEB_MODE = true;
 
 export const DEV_HOST_URL       = 'https://sb.asasconnect.com';
-export const DEV_ACTIVATION_KEY = 'BO8BFR5H9I';
+export const DEV_ACTIVATION_KEY = 'U9SNLL036A';
